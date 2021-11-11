25ae9ba104d56ea2cb72438c336c7b93e491d062
Joe Dave <joshuadavian@gmail.com>
2021-11-11 11:10:17 +0700

    camp table seeder

--- database/seeders/CampTableSeeder.php
+++ database/seeders/CampTableSeeder.php
@@ -18,19 +18,17 @@ class CampTableSeeder extends Seeder
             [
                 'title' => 'Gila Belajar',
                 'slug' => 'gila-belajar',
-                'price' => 280,
-                'created_at' => date('Y-m-d H:i:s', time()),
-                'updated_at' => date('Y-m-d H:i:s', time()),
+                'price' => 280
             ],
             [
                 'title' => 'Baru Mulai',
                 'slug' => 'baru-mulai',
-                'price' => 140,
-                'created_at' => date('Y-m-d H:i:s', time()),
-                'updated_at' => date('Y-m-d H:i:s', time()),
+                'price' => 140
             ]
         ];
 
-        Camp::insert($camps);
+        foreach ($camps as $key => $camp) {
+            Camp::create($camp);
+        }
     }
 }
